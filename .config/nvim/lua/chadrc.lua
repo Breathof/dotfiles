2a3a27a0345446bba5bb3101a134fc67a27c6a8d
Clovis Ferreira da Costa <nachofcz@gmail.com>
2024-07-22 02:18:58 +0000

    gopls and aliases updates

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -15,4 +15,89 @@ M.ui = {
 	-- },
 }
 
+M.lsp = {
+    setup_lsp = function()
+        require("nvchad_lsp").setup()
+
+        local lspconfig = require "lspconfig"
+        local null_ls = require "null-ls"
+
+
+        lspconfig.gopls.setup {
+            cmd = { "gopls" },
+            filetypes = { "go", "gomod" },
+            root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+            settings = {
+                gopls = {
+                    analyses = {
+                        unusedparams = true,
+                    },
+                    staticcheck = true,
+                },
+            },
+            on_attach = function(client, bufnr)
+                local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+                buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+                -- Mappings.
+                local opts = { noremap=true, silent=true }
+
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gl', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+                        -- Auto-format on save
+                if client.resolved_capabilities.document_formatting then
+                    vim.api.nvim_command [[augroup Format]]
+                    vim.api.nvim_command [[autocmd! * <buffer>]]
+                    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+                    vim.api.nvim_command [[augroup END]]
+                end
+
+                -- Enable completion triggered by <c-x><c-o>
+                buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+                -- Use LspAttach autocommand to only map the following keys
+                -- after the language server attaches to the current buffer
+                vim.api.nvim_create_autocmd("LspAttach", {
+                    callback = function(args)
+                        local opts = { buffer = args.buf }
+                        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                        vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
+                        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+                        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+                    end,
+                })
+            end,
+        }
+            -- Configure null-ls
+        null_ls.setup {
+            sources = {
+                null_ls.builtins.formatting.gofmt,
+                null_ls.builtins.formatting.goimports,
+                null_ls.builtins.diagnostics.golangci_lint,
+                null_ls.builtins.code_actions.gomodifytags,
+            },
+            on_attach = function(client, bufnr)
+                if client.resolved_capabilities.document_formatting then
+                    vim.api.nvim_command [[augroup Format]]
+                    vim.api.nvim_command [[autocmd! * <buffer>]]
+                    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+                    vim.api.nvim_command [[augroup END]]
+                end
+            end,
+        }
+
+    end,
+}
 return M
